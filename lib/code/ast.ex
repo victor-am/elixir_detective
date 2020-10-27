6e9d363b0ee21fe659746750481ac9298d83e21a
Victor A. Marques <victor.atmorning@gmail.com>
2020-10-28 00:00:56 +0200

    wip

--- lib/code/ast.ex
+++ lib/code/ast.ex
@@ -2,37 +2,55 @@ defmodule ElixirDetective.Code.AST do
   alias ElixirDetective.Code.ModuleReference
   alias ElixirDetective.Debug.Logger, as: Log
 
+  # Options passed down the tree.
+  # This provides a way to the information uncovered at one
+  # node influence on another node further down the tree.
+  #
+  # file_path         - pending description
+  # open_alias_block? - pending description
+  # aliases           - Lists all aliases in effect on this branch, ex: %{alias_name => aliased_module}
+  @default_options %{
+    file_path: "",
+    open_alias_block?: false,
+    aliases: %{}
+  }
+
   def find_module_references(ast_node, file_path) do
-    find(ast_node, [], file_path)
+    find(ast_node, [], %{@default_options | file_path: file_path})
   end
 
   # Defmodule node
   # When found we add pass on the module name as a namespace for next
   # nodes found in this branch of the AST.
-  defp find({:defmodule, _metadata, args} = ast_node, namespaces, file_path) do
-    reference = build_module_reference(ast_node, namespaces, file_path)
+  defp find({:defmodule, _metadata, args} = ast_node, namespaces, options) do
+    reference = build_module_reference(ast_node, namespaces, options)
     Log.defmodule_node(ast_node, reference.to)
 
     new_namespaces = Enum.concat(namespaces, reference.to)
-    continue(args, new_namespaces, file_path)
+    continue(args, new_namespaces, options)
   end
 
   # Alias node
-  defp find({:alias, _metadata, args} = ast_node, namespaces, file_path) do
+  defp find({:alias, _metadata, args} = ast_node, namespaces, options) do
     Log.alias_node(ast_node)
 
-    continue(args, namespaces, file_path)
+    continue(args, namespaces, %{options | open_alias_block?: true})
   end
 
   # Reference node
-  defp find({:__aliases__, _metadata, args} = ast_node, namespaces, file_path) do
-    reference = build_module_reference(ast_node, namespaces, file_path)
+  defp find({:__aliases__, _metadata, args} = ast_node, namespaces, options) do
+    reference = build_module_reference(ast_node, namespaces, options)
     Log.reference_node(ast_node, reference.to)
 
-    concat_reference_and_continue(args, [reference], namespaces, file_path)
+    new_options = %{
+      options |
+      open_alias_block?: false,
+      aliases: add_aliases_if_block_is_open(options, reference)
+    }
+    concat_reference_and_continue(args, [reference], namespaces, new_options)
   end
 
-  # Reference with multiple modules node
+  # Reference using another module as namespace with multiple modules
   # Example:
   # alias Foo.{Bar, Batz}
   defp find(
@@ -45,20 +63,25 @@ defmodule ElixirDetective.Code.AST do
             }
           ]} = _ast_node,
          namespaces,
-         file_path
+         options
        )
        when is_list(args) do
     references =
       Enum.map(args, fn arg_node ->
-        reference = build_module_reference(arg_node, namespaces, file_path, alias_namespaces)
+        reference = build_module_reference(arg_node, namespaces, options, alias_namespaces)
         Log.reference_node(arg_node, reference.to)
         reference
       end)
 
-    concat_reference_and_continue([], references, namespaces, file_path)
+    new_options = %{
+      options |
+      open_alias_block?: false,
+      aliases: add_aliases_if_block_is_open(options, references)
+    }
+    concat_reference_and_continue([], references, namespaces, new_options)
   end
 
-  # Reference with multiple modules node
+  # Reference using __MODULE__ as namespace with multiple modules
   # Example:
   # alias __MODULE__.{Bar, Batz}
   defp find(
@@ -71,45 +94,50 @@ defmodule ElixirDetective.Code.AST do
             }
           ]} = _ast_node,
          namespaces,
-         file_path
+         options
        )
        when is_list(args) do
     references =
       Enum.map(args, fn arg_node ->
-        reference = build_module_reference(arg_node, namespaces, file_path, namespaces)
+        reference = build_module_reference(arg_node, namespaces, options, namespaces)
         Log.reference_node(arg_node, reference.to)
         reference
       end)
 
-    concat_reference_and_continue([], references, namespaces, file_path)
+    new_options = %{
+      options |
+      open_alias_block?: false,
+      aliases: add_aliases_if_block_is_open(options, references)
+    }
+    concat_reference_and_continue([], references, namespaces, new_options)
   end
 
   # Do node
-  defp find([do: {_token, _, _args} = arg] = ast_node, namespaces, file_path) do
+  defp find([do: {_token, _, _args} = arg] = ast_node, namespaces, options) do
     Log.do_node(ast_node)
 
-    continue([arg], namespaces, file_path)
+    continue([arg], namespaces, options)
   end
 
   # Block node
-  defp find({:__block__, _meta, args} = ast_node, namespaces, file_path) do
+  defp find({:__block__, _meta, args} = ast_node, namespaces, options) do
     Log.block_node(ast_node)
 
-    continue(args, namespaces, file_path)
+    continue(args, namespaces, options)
   end
 
   # Call node
-  defp find({{:., _meta1, args1}, _meta2, args2} = ast_node, namespaces, file_path) do
+  defp find({{:., _meta1, args1}, _meta2, args2} = ast_node, namespaces, options) do
     Log.call_node(ast_node)
 
-    continue(args1 ++ [args2], namespaces, file_path)
+    continue(args1 ++ [args2], namespaces, options)
   end
 
   # Unknown node
-  defp find({_token, _metadata, args} = ast_node, namespaces, file_path) do
+  defp find({_token, _metadata, args} = ast_node, namespaces, options) do
     Log.unknown_node(ast_node)
 
-    continue(args, namespaces, file_path)
+    continue(args, namespaces, options)
   end
 
   # This matches dead-ends in the tree
@@ -142,7 +170,7 @@ defmodule ElixirDetective.Code.AST do
     [{:__MODULE__, meta2, nil}]
   end
 
-  defp build_module_reference(node, current_module, file_path, reference_extra_namespace \\ []) do
+  defp build_module_reference(node, current_module, options, reference_extra_namespace \\ []) do
     module_name =
       node
       |> extract_module_name()
@@ -154,13 +182,25 @@ defmodule ElixirDetective.Code.AST do
       |> replace__MODULE__mentions(current_module)
       |> Enum.concat(module_name)
 
+    # TODO: Extract this to somewhere else
+    # this logic handles aliased modules like:
+    # alias Foo.Bar
+    # Bar.Batz.some_function()
+    root_module_from_name = List.first(module_full_name)
+    matched_alias = options.aliases[root_module_from_name]
+    module_full_name = if matched_alias do
+      matched_alias ++ tl(module_full_name)
+    else
+      module_full_name
+    end
+
     {_token, [line: line_of_code], _args} = node
 
     ModuleReference.build(%{
       from: current_module,
       to: module_full_name,
       line: line_of_code,
-      file_path: file_path
+      file_path: options.file_path
     })
   end
 
@@ -177,15 +217,33 @@ defmodule ElixirDetective.Code.AST do
     |> List.flatten()
   end
 
-  defp continue(args, namespaces, file_path) do
+  defp continue(args, namespaces, options) do
     args
     |> List.wrap()
-    |> Enum.flat_map(fn node -> find(node, namespaces, file_path) end)
+    |> Enum.flat_map(fn node -> find(node, namespaces, options) end)
   end
 
-  defp concat_reference_and_continue(args, references, namespaces, file_path) do
+  defp concat_reference_and_continue(args, references, namespaces, options) do
     args
-    |> continue(namespaces, file_path)
+    |> continue(namespaces, options)
     |> Enum.concat(references)
+  end
+
+  require IEx
+  # If the block is closed, do nothing
+  defp add_aliases_if_block_is_open(%{open_alias_block?: false, aliases: aliases}, _references) do
+    IEx.pry()
+    aliases
+  end
+
+  defp add_aliases_if_block_is_open(options, references) when is_list(references) do
+    new_aliases = for ref <- references, into: %{}, do: {List.last(ref.to), ref.to}
+    IEx.pry()
+    Map.merge(options.aliases, new_aliases)
+  end
+
+  defp add_aliases_if_block_is_open(options, reference) do
+    IEx.pry()
+    Map.put(options.aliases, List.last(reference.to), reference.to)
   end
 end
--- test/fixtures/alias_example.exs
+++ test/fixtures/alias_example.exs
@@ -1,12 +1,7 @@
 defmodule SimpleModule do
-  alias OtherModule
-  alias MyNamespace.YetAnotherModule, as: Mod
   alias MyNamespace.{Module1, Module2}
-  alias __MODULE__.Module3
-  alias __MODULE__.{Module4, Module5}
 
   def function do
     Module1.Module6.my_function()
-    Mod.Module7.another_function(true)
   end
 end
